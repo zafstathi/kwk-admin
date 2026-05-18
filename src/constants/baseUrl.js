c37a53e42eeefb1e258fac01a9b8b97d36297026
Stathi Zafiratos <zafstathi@gmail.com>
2026-05-17 23:19:01 -0400

    Switch to dart-sass, point API to api.kwknet.com

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,3 +1,2 @@
-export const baseUrl = "http://13.53.131.3:3001/api/";
-//export const API_URL = "http://localhost:3001/api/";
-// export const API_URL = "https://kwk-api.devjunction.xyz/api/";
+export const baseUrl = process.env.REACT_APP_API_URL || "https://api.kwknet.com/api/";
+export const API_URL = baseUrl;
